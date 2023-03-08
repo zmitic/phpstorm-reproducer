9a64256099b2818464d473dd482df15410e5f255
zeljko <mitke013@gmail.com>
2023-03-08 12:23:24 +0100

    Initial commit

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+class ProductController
+{
+    public function __invoke(): string
+    {
+        return 'Future response';
+    }
+}
